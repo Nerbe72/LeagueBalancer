9a80f355a4c702973242622f44bfc71757bc6dea
qpdh <74190329+qpdh@users.noreply.github.com>
2021-03-25 19:49:45 +0900

    PPADS 압축해제
    ㅈㄱㄴ

---
+++ Learn_JHM/PPADS/pyFile/playerInfo.py
@@ -0,0 +1,9 @@
+def output(name):
+    for i in name:
+        print("{0}".format(i))
+
+def main():
+    name = ["김동현", "이호진", "정회민", "최성원"]
+    output(name)
+    
+main()
---
+++ Learn_JHM/PPADS/src/main/PPADS.java
@@ -0,0 +1,12 @@
+package main;
+
+import execWork.PlayPy;
+
+public class PPADS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PlayPy pp = new PlayPy();
+	}
+
+}
