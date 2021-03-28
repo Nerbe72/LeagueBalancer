1ccebf77c9b64d8e5ccfb0605055e6ea66ae5666
qpdh <74190329+qpdh@users.noreply.github.com>
2021-03-29 00:02:06 +0900

    Java X Android TCP Networking
    Chat

--- Learn_KDH/Android/Test/app/src/main/java/kr/ac/kpu/LeagueBalance/MainActivity.java
+++ Learn_KDH/Android/Test/app/src/main/java/kr/ac/kpu/LeagueBalance/MainActivity.java
@@ -1,34 +1,119 @@
 package kr.ac.kpu.LeagueBalance;
 
+import androidx.annotation.RestrictTo;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.renderscript.ScriptGroup;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    Button button;
-    TextView textView;
-    Handler handler = new Handler();
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.Scanner;
 
-    @Override
+public class MainActivity extends Activity {
+    private Socket socket;
+    PrintWriter out;
+    BufferedReader in;
+    EditText input;
+    Button button;
+    TextView recvView, sendView;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
-        textView = (TextView) findViewById(R.id.textView);
-        ClientSocket clientSocket = new ClientSocket(textView, handler);
+        button = (Button) findViewById(R.id.connectButton);
+        input = (EditText) findViewById(R.id.editText);
+        recvView = (TextView) findViewById(R.id.RecvView);
+        sendView = (TextView) findViewById(R.id.SendView);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                clientSocket.start();
-            }
-        });
+
     }
 
+    public void ClientSocketOpen(View view) throws IOException {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    socket = new Socket();
+                    SocketAddress addr = new InetSocketAddress("192.168.219.100", 9900);
+                    socket.connect(addr);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    out = new PrintWriter(socket.getOutputStream(), true);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
+                while (true) {
+                    String msg;
+                    try {
+                        msg = in.readLine();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                recvView.setText("[RECV] : " + msg);
+
+                            }
+                        });
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }).start();
+    }
+
+    public void SendMessage(View view) {
+        if (out == null) return;
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String msg = input.getText().toString();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String msg = input.getText().toString();
+                        sendView.setText("[SEND]" + msg);
+                        input.setText("");
+                    }
+                });
+                out.println(msg);
+            }
+        }).start();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- Learn_KDH/Java/src/NetWork_Programming/Server_Socket.java
+++ Learn_KDH/Java/src/NetWork_Programming/Server_Socket.java
@@ -8,34 +8,42 @@ public class Server_Socket {
 
 	public static void main(String[] args) {
 		BufferedReader in = null;
-		BufferedWriter out = null;
+		PrintWriter out = null;
 		ServerSocket listener = null;
 		Socket socket = null;
 
 		Scanner scanner = new Scanner(System.in);
 
 		try {
-			listener = new ServerSocket(9999);
+			listener = new ServerSocket(9900);
 			System.out.println("연결을 기다리고 있습니다.......");
+			
 			socket = listener.accept();
 			System.out.println("연결 되었습니다.");
+			
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			out = new PrintWriter(socket.getOutputStream(),true);
+			
 			while (true) {
 				String inputMessage = in.readLine();
+				
+				
 				if (inputMessage.equalsIgnoreCase("bye")) {
-					System.out.println("클라이어늩에서 bye로 연결을 종료하였음");
+					System.out.println("클라이언트에서 bye로 연결을 종료하였음");
 					break;
 				}
+				
 				System.out.println("클라이언트 : " + inputMessage);
-				System.out.println("보내기>> ");
+				System.out.print("보내기>> ");
 				String outputMessage = scanner.nextLine();
 				out.write(outputMessage + "\n");
 				out.flush();
 			}
+			
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
+		
 		try {
 			scanner.close();
 			socket.close();
