b1d3843a57ecc39b9e95b4447a633116d5a71d53
Nerbe72 <nerb0702@gmail.com>
2021-03-30 01:01:55 +0900

    Get ingame id's kda
    Get match player id's kda

--- Learn_LHJ/API.py
+++ Learn_LHJ/API.py
@@ -1,53 +1,73 @@
-import requests #api를 불러오는 모듈
+import requests  # api를 불러오는 모듈
 import pandas as pd
-import gc #가비지 컬렉터
-import json
-import time #sleep 함수를 사용하기 위함 / 좋지 않으므로 대체제 필요
+from collections import defaultdict
+import time  # sleep 함수를 사용하기 위함 / 좋지 않으므로 대체제 필요
 
 REGION = "kr"
 
-def set_URL(query, req, region=REGION):
-    assembled = "https://"+region+".api.riotgames.com/lol/"+query+req+'?api_key='+api_key
+
+def set_url(query, req, option=''):
+    assembled = "https://kr.api.riotgames.com/lol/" + query + req + '?api_key=' + api_key + option
     return assembled
 
 
-api_key = "RGAPI-1d071ed1-8235-416a-87af-7f6a34b5c04c"
+api_key = "RGAPI-fa88326b-e677-4dfa-b709-8417329ea2df"
 
 name = input("소환사명 : ")
 season = str(13)
 
-
 try:
-    summoner = set_URL("summoner/v4/summoners/by-name/", name)
+    summoner = set_url("summoner/v4/summoners/by-name/", name)
     res = requests.get(summoner)
-    accountid = res.json()['accountId']
+    account = res.json()['accountId']
+    print('Encrypted Account ID : ', account)
 except:
     print("소환사 데이터 획득 오류")
 
 try:
-    match = set_URL("match/v4/matchlists/by-account/", accountid)
+    match = set_url("match/v4/matchlists/by-account/", account, '&queue=420&endIndex=30')  # queue=430&
     res = requests.get(match)
     temp = pd.DataFrame(res.json()['matches'])
-    match_info = pd.DataFrame(temp.loc[((temp['queue'] == 420) ^ (temp['queue'] == 430)), ['gameId', 'lane']]).reset_index()
+    temp = temp[temp['lane'] != 'NONE']
+    match_info = pd.DataFrame(temp.loc[:, ['gameId', 'lane']]).reset_index(drop=True)
     print(match_info)
     MainLane = match_info['lane'].value_counts()[0:2]
-    print(MainLane)
-    print(MainLane.index.name)
-    print("MostLane : ")
+    print('Game count : ', len(match_info))
+    print("MostLane : ", end='')
+    for i in range(2):
+        print(MainLane.index[i], end=' ')
+    print()
 except:
     print("매칭 데이터 획득 오류")
 
-try:
-    for i in match_info.index:
-        matchid = match_info.get_value(i, 'gameId')
-        print(matchid)
-        game = set_URL("match/v4/matches/", matchid)
-        print(game)
-        res = requests.get(game)
-except:
-    print("통계 획득 오류")
+game_info = pd.DataFrame()
+game_id = match_info['gameId'].values.tolist()
 
+print(id)
 
+games = defaultdict(int)
+id = 0
+for i in game_id:
+    game = set_url("match/v4/matches/", str(i))
+    res = requests.get(game)
+    game_temp = res.json()
 
+    try:
+        ids = game_temp['participantIdentities']
+        for i in ids:
+            if (i['player']['summonerName'] == name):
+                id = i['participantId']
+                print(id)
+    except:
+        print('인게임 플레이어 ID 호출 오류')
 
+    games[game_temp['gameId']] = game_temp['participants'][id-1]['stats']
+
+games = pd.DataFrame(games)
+games = games.transpose()
+games = games.reset_index(drop=True)
+games = games.loc[:, ['kills', 'deaths', 'assists']]
+games['kda'] = (games['kills'] + games['assists']) / games['deaths']
+
+print(games)
 
